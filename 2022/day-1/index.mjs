7a57e049304926bea7ed7aef8d5d0b7fd695b080
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-02 01:11:47 +0330

    feat: solve part two of day 1

--- 2022/day-1/index.mjs
+++ 2022/day-1/index.mjs
@@ -11,12 +11,30 @@ elvesItemCalorieList = elvesItemCalorieList.map((list) => {
 
 elvesItemCalorieList = elvesItemCalorieList.filter((list) => list);
 
-let maxSum = 0;
+let sumList = [];
 elvesItemCalorieList.forEach((list) => {
   let sum = 0;
   list.forEach((num) => {
     sum += +num;
   });
-  if (maxSum < sum) maxSum = sum;
+  sumList.push(sum);
 });
-console.log(maxSum);
+
+sumList = sumList.sort(function (a, b) {
+  return a - b;
+});
+console.log('The first highest Sum of Elves calories: %s', sumList[sumList.length - 1]);
+console.log('The second highest Sum of Elves calories: %s', sumList[sumList.length - 2]);
+console.log('The thirs highest Sum of Elves calories: %s', sumList[sumList.length - 3]);
+
+console.log('\n');
+
+console.log(
+  '[Part-1] The first highest Sum of Elves calories: %s',
+  sumList[sumList.length - 1] + sumList[sumList.length - 2] + sumList[sumList.length - 3]
+);
+
+console.log(
+  '[Part-2] Top Sum of three Elves carrying the most Calories: %s',
+  sumList[sumList.length - 1] + sumList[sumList.length - 2] + sumList[sumList.length - 3]
+);
