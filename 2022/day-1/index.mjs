8e2c90333f84a3b807e2a7c84d0f3d11c7b046fc
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-03 10:23:14 +0330

    refactor: part 1

--- 2022/day-1/index.mjs
+++ 2022/day-1/index.mjs
@@ -2,6 +2,14 @@
 
 import {readFileSync} from 'fs';
 
+function calcuteElveCaloriesSum(calorieList) {
+  let sum = 0;
+  for (let i = 0; calorieList.length > i; i++) {
+    sum += +calorieList[i];
+  }
+  return sum;
+}
+
 const fileContent = readFileSync('./input.txt', 'utf8');
 
 let elvesItemCalorieList = fileContent.split(/^\n/gm);
@@ -9,32 +17,17 @@ elvesItemCalorieList = elvesItemCalorieList.map((list) => {
   return list.split(/\n/gm);
 });
 
-elvesItemCalorieList = elvesItemCalorieList.filter((list) => list);
-
 let sumList = [];
-elvesItemCalorieList.forEach((list) => {
-  let sum = 0;
-  list.forEach((num) => {
-    sum += +num;
-  });
-  sumList.push(sum);
-});
+for (let i = 0; elvesItemCalorieList.length > i; i++) {
+  sumList[i] = calcuteElveCaloriesSum(elvesItemCalorieList[i]);
+}
 
 sumList = sumList.sort(function (a, b) {
   return a - b;
 });
-console.log('The first highest Sum of Elves calories: %s', sumList[sumList.length - 1]);
-console.log('The second highest Sum of Elves calories: %s', sumList[sumList.length - 2]);
-console.log('The thirs highest Sum of Elves calories: %s', sumList[sumList.length - 3]);
 
-console.log('\n');
-
+console.log('Part One Response: [%s]', sumList[sumList.length - 1]);
 console.log(
-  '[Part-1] The first highest Sum of Elves calories: %s',
-  sumList[sumList.length - 1] + sumList[sumList.length - 2] + sumList[sumList.length - 3]
-);
-
-console.log(
-  '[Part-2] Top Sum of three Elves carrying the most Calories: %s',
+  'Part Two Response: [%s]',
   sumList[sumList.length - 1] + sumList[sumList.length - 2] + sumList[sumList.length - 3]
 );
