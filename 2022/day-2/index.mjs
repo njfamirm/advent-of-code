4981f3d1a91ae410e998b3c032a18cebb9223067
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-03 10:17:02 +0330

    feat: solve part two of day 2

--- 2022/day-2/index.mjs
+++ 2022/day-2/index.mjs
@@ -2,37 +2,62 @@
 
 import {readFileSync} from 'fs';
 
-const file = readFileSync('./input.txt', 'utf8');
-
-const roundList = file.split(/\n/gm).map(r => r.split(' '));
-
-let pointSum=0
-for (let i=0; roundList.length > i; i++) {
-  pointSum += calcuteRoundPoint(roundList[i])
-}
-
-console.log(pointSum)
-
-function calcuteRoundPoint(moveList) {
+function calcutePartOneRoundPoint(moveList) {
   let point = 0;
   switch (moveList[1]) {
     case 'X':
-      point+=1
-      if (moveList[0] === 'A') point+=3
-      else if (moveList[0] === 'C') point+=6
+      point += 1;
+      if (moveList[0] === 'A') point += 3;
+      else if (moveList[0] === 'C') point += 6;
       break;
     case 'Y':
-      point+=2
-      if (moveList[0] === 'B') point+=3
-      else if (moveList[0] === 'A') point+=6
-      break
+      point += 2;
+      if (moveList[0] === 'B') point += 3;
+      else if (moveList[0] === 'A') point += 6;
+      break;
     case 'Z':
-      point+=3
-      if (moveList[0] === 'C') point+=3
-      else if (moveList[0] === 'B') point+=6
-      break
+      point += 3;
+      if (moveList[0] === 'C') point += 3;
+      else if (moveList[0] === 'B') point += 6;
+      break;
   }
-  console.log(point)
   return point;
 }
 
+function calcutePartTwoRoundPoint(moveList) {
+  let point = 0;
+  switch (moveList[1]) {
+    case 'X':
+      if (moveList[0] === 'A') point += 3;
+      else if (moveList[0] === 'B') point += 1;
+      else point += 2;
+      break;
+    case 'Y':
+      point += 3;
+      if (moveList[0] === 'A') point += 1;
+      else if (moveList[0] === 'B') point += 2;
+      else point += 3;
+      break;
+    case 'Z':
+      point += 6;
+      if (moveList[0] === 'A') point += 2;
+      else if (moveList[0] === 'B') point += 3;
+      else point += 1;
+      break;
+  }
+  return point;
+}
+
+const file = readFileSync('./input.txt', 'utf8');
+
+const roundList = file.split(/\n/gm).map((r) => r.split(' '));
+
+let partOnePointSum = 0;
+let partTwoPointSum = 0;
+for (let i = 0; roundList.length > i; i++) {
+  partOnePointSum += calcutePartOneRoundPoint(roundList[i]);
+  partTwoPointSum += calcutePartTwoRoundPoint(roundList[i]);
+}
+
+console.log('Part One Response: [%s]', partOnePointSum);
+console.log('Part Two Response: [%s]', partTwoPointSum);
